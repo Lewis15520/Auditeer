f73a3966a939e89d0ad3e92c9d8c0f891b614466
Lewis Hayter <Lewis15520@users.noreply.github.com>
2020-11-08 21:18:07 +0000

    Multi-update
    1. Add migration publishing | 2. Add Traits namespace to allow access to main app models

--- src/app/Providers/AuditeerServiceProvider.php
+++ src/app/Providers/AuditeerServiceProvider.php
@@ -9,7 +9,13 @@ class AuditeerServiceProvider extends ServiceProvider
 
     public function boot()
     {
-
+        if ($this->app->runningInConsole()) {
+            if(! class_exists('CreateAuditLogTable')) {
+                $this->publishes([
+                    __DIR__ . '/../../database/migrations/create_audit_log_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_audit_log_table.php'),
+                ], 'migrations');
+            }
+        }
     }
 
     public function register()
